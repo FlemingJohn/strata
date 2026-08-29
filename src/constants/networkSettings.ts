ac5566a2b6ea1c18013029961a71d7be0b2380c1
FlemingJohn <flemjohn08@gmail.com>
2026-08-29 21:55:32 +0530

    Add Creditcoin network addresses and endpoints

---
+++ src/constants/networkSettings.ts
@@ -0,0 +1,21 @@
+export const CREDITCOIN_TESTNET_RPC_URL = "https://rpc.cc3-testnet.creditcoin.network";
+export const CREDITCOIN_TESTNET_CHAIN_ID = 102031;
+
+export const PROOF_BUILDER_API_URL = "https://proof-gen-api.cc3-testnet.creditcoin.network";
+export const ATTESTATION_DASHBOARD_URL = "https://dashboard.cc3-testnet.creditcoin.network";
+
+export const BLOCK_PROVER_PRECOMPILE_ADDRESS = "0x0000000000000000000000000000000000000FD2";
+export const CHAIN_INFO_PRECOMPILE_ADDRESS = "0x0000000000000000000000000000000000000fd3";
+export const TRANSACTION_DECODER_ADDRESS = "0x731c345d79Fb8BbDC541f9DF3b6317585F849F9f";
+
+export const ETHEREUM_MAINNET_CHAIN_KEY = 3;
+export const ETHEREUM_SEPOLIA_CHAIN_KEY = 1;
+
+export const MAXIMUM_PROOFS_PER_BATCH = 10;
+export const MAXIMUM_BLOCK_RANGE_PER_BATCH = 1000;
+
+export const ETHERSCAN_API_URL = "https://api.etherscan.io/api";
+export const ETHERSCAN_TRANSACTION_URL = "https://etherscan.io/tx";
+
+export const TRANSACTIONS_TO_PROVE_PER_WALLET = 8;
+export const PROOF_POLLING_INTERVAL_MILLISECONDS = 5000;
