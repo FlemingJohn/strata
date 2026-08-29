271263b393f269f993501702a8d1e426641e5d14
FlemingJohn <flemjohn08@gmail.com>
2026-08-29 22:54:06 +0530

    Add tileset paths and the chosen floor and wall tiles

---
+++ src/constants/tilesetSettings.ts
@@ -0,0 +1,16 @@
+export const FLOOR_TILESET_PATH = "/assets/pixelCrawler/Environment/Tilesets/Floors_Tiles.png";
+export const WALL_TILESET_PATH = "/assets/pixelCrawler/Environment/Tilesets/Wall_Tiles.png";
+
+export const TILESET_COLUMNS = 25;
+export const TILE_SIZE = 16;
+
+export const FLOOR_TILE_COLUMN = 17;
+export const FLOOR_TILE_ROW = 2;
+
+export const WALL_TILE_COLUMN = 14;
+export const WALL_TILE_ROW = 2;
+
+export const EXIT_TILE_COLUMN = 17;
+export const EXIT_TILE_ROW = 3;
+
+export const COMBAT_DISPLAY_SCALE = 3;
