a261a611fbf08d84423e247191d254584b4db0d0
FlemingJohn <flemjohn08@gmail.com>
2026-08-29 21:55:31 +0530

    Add relic and relic effect types

---
+++ src/types/relic.ts
@@ -0,0 +1,48 @@
+export type RelicName =
+  | "frontierEmber"
+  | "cursedSherd"
+  | "apeMask"
+  | "warHelm"
+  | "vaultSeal"
+  | "forkCairn"
+  | "driftStone"
+  | "ashShard";
+
+export type RelicOrigin =
+  | "failedTransaction"
+  | "highGasTransaction"
+  | "tokenMint"
+  | "veryOldTransaction"
+  | "largeValueTransfer"
+  | "contractCreation"
+  | "plainTransfer"
+  | "burnTransfer";
+
+export interface RelicEffect {
+  damageDealtMultiplier: number;
+  damageTakenMultiplier: number;
+  movementSpeedMultiplier: number;
+  shieldGrantedPerRoom: number;
+  healthRestoredPerKill: number;
+  rollCostsNoStaminaAboveHalfHealth: boolean;
+  attacksPassThroughEnemies: boolean;
+  reviveOnceEachRun: boolean;
+  spawnAllyEveryFifthHit: boolean;
+}
+
+export interface RelicDefinition {
+  name: RelicName;
+  displayName: string;
+  origin: RelicOrigin;
+  description: string;
+  weaponSpriteIndex: number;
+  effect: RelicEffect;
+}
+
+export interface EquippedRelic {
+  definition: RelicDefinition;
+  sourceTransactionHash: string;
+  sourceBlockNumber: number;
+  sourceYear: number;
+  stratumNumber: number;
+}
