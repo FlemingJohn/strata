dcabd8489cdcb35a98e110e0820512e7f4b96a4b
FlemingJohn <flemjohn08@gmail.com>
2026-08-29 21:55:31 +0530

    Add room layout and floor description types

---
+++ src/types/room.ts
@@ -0,0 +1,30 @@
+import type { EnemyName } from "./enemy";
+
+export type TileKind = "floor" | "wall" | "cursed" | "exit";
+
+export interface RoomLayout {
+  columnCount: number;
+  rowCount: number;
+  tiles: TileKind[][];
+  playerStartColumn: number;
+  playerStartRow: number;
+  exitColumn: number;
+  exitRow: number;
+}
+
+export interface EnemySpawn {
+  enemyName: EnemyName;
+  horizontalPosition: number;
+  verticalPosition: number;
+}
+
+export interface FloorDescription {
+  floorNumber: number;
+  sourceBlockNumber: number;
+  roomCount: number;
+  difficultyMultiplier: number;
+  eliteCount: number;
+  cursedTileCount: number;
+  treasureTier: number;
+  layoutSeed: string;
+}
