e23ecc6921bd95876ac7a2825e7304555cc85dae
FlemingJohn <flemjohn08@gmail.com>
2026-08-29 22:48:57 +0530

    Add dungeon grid and generation tuning values

---
+++ src/constants/dungeonSettings.ts
@@ -0,0 +1,19 @@
+export const FLOOR_GRID_COLUMNS = 5;
+export const FLOOR_GRID_ROWS = 5;
+
+export const MINIMUM_ROOMS_PER_FLOOR = 6;
+export const MAXIMUM_ROOMS_PER_FLOOR = 12;
+
+export const ROOM_TILE_COLUMNS = 20;
+export const ROOM_TILE_ROWS = 15;
+
+export const MINIMUM_ENEMIES_PER_ROOM = 2;
+export const MAXIMUM_ENEMIES_PER_ROOM = 8;
+export const ENEMY_INCREASE_PER_FLOOR = 0.7;
+
+export const DIFFICULTY_INCREASE_PER_FLOOR = 0.12;
+export const OBSTACLE_BLOCKS_MINIMUM = 3;
+export const OBSTACLE_BLOCKS_MAXIMUM = 7;
+
+export const BUSYNESS_ROOM_CONTRIBUTION = 0.5;
+export const TREASURE_TIER_COUNT = 4;
