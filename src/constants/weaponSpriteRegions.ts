95c144784f4f1329b90cbc337a8fad615ea6c987
FlemingJohn <flemjohn08@gmail.com>
2026-08-29 22:37:52 +0530

    Add measured crop regions for eight weapon icons

---
+++ src/constants/weaponSpriteRegions.ts
@@ -0,0 +1,17 @@
+import type { SpriteCropRegion } from "../types/spriteSheet";
+
+export const WOOD_WEAPON_SHEET_PATH = "/assets/pixelCrawler/Weapons/Wood/Wood.png";
+
+export const WEAPON_ICON_BOX_SIZE = 32;
+export const WEAPON_ICON_DISPLAY_SCALE = 3;
+
+export const WEAPON_SPRITE_REGIONS: SpriteCropRegion[] = [
+  { left: 119, top: 19, width: 17, height: 24 },
+  { left: 51, top: 2, width: 11, height: 13 },
+  { left: 67, top: 50, width: 29, height: 28 },
+  { left: 112, top: 0, width: 32, height: 16 },
+  { left: 145, top: 0, width: 14, height: 16 },
+  { left: 48, top: 18, width: 16, height: 28 },
+  { left: 0, top: 50, width: 16, height: 28 },
+  { left: 32, top: 49, width: 16, height: 31 }
+];
