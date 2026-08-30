c518a179a60b8aea2498d352e701797b229e9df8
FlemingJohn <flemjohn08@gmail.com>
2026-08-30 21:08:47 +0530

    Add fire placement and ember types

---
+++ src/types/fire.ts
@@ -0,0 +1,14 @@
+export interface FirePlacement {
+  horizontalPosition: number;
+  verticalPosition: number;
+  flickerPhase: number;
+}
+
+export interface Ember {
+  horizontalPosition: number;
+  verticalPosition: number;
+  velocityHorizontal: number;
+  velocityVertical: number;
+  secondsRemaining: number;
+  totalSeconds: number;
+}
