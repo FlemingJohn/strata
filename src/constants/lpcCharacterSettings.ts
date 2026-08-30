57a1fdd6219ed95a5e72424065e268410019c01e
FlemingJohn <flemjohn08@gmail.com>
2026-08-30 21:13:29 +0530

    Add the spellcast animation

--- src/constants/lpcCharacterSettings.ts
+++ src/constants/lpcCharacterSettings.ts
@@ -1,8 +1,6 @@
 export const LPC_FOLDER = "/assets/lpc";
 export const LPC_FRAME_SIZE = 64;
 
-export const LPC_LAYER_ORDER = ["body", "head", "legs", "feet", "torso", "hood"];
-
 export const LPC_ROW_FACING_UP = 0;
 export const LPC_ROW_FACING_LEFT = 1;
 export const LPC_ROW_FACING_DOWN = 2;
@@ -17,7 +15,8 @@ export type LpcAnimationName =
   | "slash"
   | "backslash"
   | "thrust"
-  | "hurt";
+  | "hurt"
+  | "spellcast";
 
 export const LPC_ANIMATIONS: LpcAnimationName[] = [
   "idle",
@@ -25,7 +24,8 @@ export const LPC_ANIMATIONS: LpcAnimationName[] = [
   "slash",
   "backslash",
   "thrust",
-  "hurt"
+  "hurt",
+  "spellcast"
 ];
 
 export const LPC_FRAMES_PER_SECOND: Record<LpcAnimationName, number> = {
@@ -34,5 +34,6 @@ export const LPC_FRAMES_PER_SECOND: Record<LpcAnimationName, number> = {
   slash: 14,
   backslash: 14,
   thrust: 14,
-  hurt: 8
+  hurt: 8,
+  spellcast: 10
 };
