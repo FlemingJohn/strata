60a2c60fca1dac890552faabb5a102f24a387125
FlemingJohn <flemjohn08@gmail.com>
2026-08-30 20:09:29 +0530

    Add the body bands and colours used to dress the hero

---
+++ src/constants/heroClothingSettings.ts
@@ -0,0 +1,14 @@
+export const TORSO_TOP_IN_FRAME = 31;
+export const TORSO_BOTTOM_IN_FRAME = 41;
+export const LEGS_TOP_IN_FRAME = 41;
+export const LEGS_BOTTOM_IN_FRAME = 48;
+export const BELT_TOP_IN_FRAME = 39;
+export const BELT_BOTTOM_IN_FRAME = 41;
+
+export const DEFAULT_TUNIC_COLOUR = "#4A7CA0";
+export const TROUSERS_COLOUR = "#3A2A1C";
+export const BELT_COLOUR = "#5E4326";
+
+export const CLOTHING_OPACITY = 0.82;
+export const TROUSERS_OPACITY = 0.9;
+export const BELT_OPACITY = 0.95;
