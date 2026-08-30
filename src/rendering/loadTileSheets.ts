831539085e0b95d32d99c3d842667fa2b81447aa
FlemingJohn <flemjohn08@gmail.com>
2026-08-30 21:08:47 +0530

    Load the dungeon, fire and smoke sheets

--- src/rendering/loadTileSheets.ts
+++ src/rendering/loadTileSheets.ts
@@ -1,8 +1,16 @@
-import { FLOOR_TILESET_PATH, WALL_TILESET_PATH } from "../constants/tilesetSettings";
+import {
+  DUNGEON_TILESET_PATH,
+  FLOOR_TILESET_PATH,
+  WALL_TILESET_PATH
+} from "../constants/tilesetSettings";
+import { FIRE_SHEET_PATH, SMOKE_SHEET_PATH } from "../constants/fireSettings";
 
 export interface TileSheets {
   floorSheet: HTMLImageElement;
   wallSheet: HTMLImageElement;
+  dungeonSheet: HTMLImageElement;
+  fireSheet: HTMLImageElement;
+  smokeSheet: HTMLImageElement;
 }
 
 function loadImage(path: string): Promise<HTMLImageElement> {
@@ -15,10 +23,13 @@ function loadImage(path: string): Promise<HTMLImageElement> {
 }
 
 export async function loadTileSheets(): Promise<TileSheets> {
-  const [floorSheet, wallSheet] = await Promise.all([
+  const [floorSheet, wallSheet, dungeonSheet, fireSheet, smokeSheet] = await Promise.all([
     loadImage(FLOOR_TILESET_PATH),
-    loadImage(WALL_TILESET_PATH)
+    loadImage(WALL_TILESET_PATH),
+    loadImage(DUNGEON_TILESET_PATH),
+    loadImage(FIRE_SHEET_PATH),
+    loadImage(SMOKE_SHEET_PATH)
   ]);
 
-  return { floorSheet, wallSheet };
+  return { floorSheet, wallSheet, dungeonSheet, fireSheet, smokeSheet };
 }
