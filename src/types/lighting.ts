da0a894e44ef6e82647c74b67b25b657bb71fe36
FlemingJohn <flemjohn08@gmail.com>
2026-08-30 20:05:08 +0530

    Add light source and torch placement types

---
+++ src/types/lighting.ts
@@ -0,0 +1,12 @@
+export interface LightSource {
+  horizontalPosition: number;
+  verticalPosition: number;
+  radiusInPixels: number;
+  flickerPhase: number;
+}
+
+export interface TorchPlacement {
+  horizontalPosition: number;
+  verticalPosition: number;
+  flickerPhase: number;
+}
