2e10e78ecc9935704a57265203d19191ee3b665b
FlemingJohn <flemjohn08@gmail.com>
2026-09-02 19:31:11 +0530

    Mute the music with the same key

--- src/audio/sharedSoundEngine.ts
+++ src/audio/sharedSoundEngine.ts
@@ -1,6 +1,7 @@
 import type { SoundEngine } from "../types/sound";
 import { MUTE_KEY } from "../constants/soundSettings";
 import { createSoundEngine } from "./createSoundEngine";
+import { findMusicEngine } from "./sharedMusicEngine";
 
 let sharedEngine: SoundEngine | null = null;
 let hasAttachedMuteKey = false;
@@ -8,13 +9,14 @@ let hasAttachedMuteKey = false;
 export function findSoundEngine(): SoundEngine {
   if (!sharedEngine) {
     sharedEngine = createSoundEngine();
+    findMusicEngine().setMuted(sharedEngine.isMuted());
   }
 
   if (!hasAttachedMuteKey) {
     hasAttachedMuteKey = true;
     window.addEventListener("keydown", (event) => {
       if (event.key.toLowerCase() === MUTE_KEY && sharedEngine) {
-        sharedEngine.toggleMute();
+        findMusicEngine().setMuted(sharedEngine.toggleMute());
       }
     });
   }
