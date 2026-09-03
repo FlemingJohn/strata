77df9568143ba77194ed2026fd69ccd54739928b
FlemingJohn <flemjohn08@gmail.com>
2026-09-03 21:31:37 +0530

    Add the relics a practice run hands you

---
+++ src/constants/practiceRunSettings.ts
@@ -0,0 +1,14 @@
+import type { RelicName } from "../types/relic";
+
+export const PRACTICE_RELIC_NAMES: RelicName[] = [
+  "firstLight",
+  "warHelm",
+  "ashShard",
+  "driftStone",
+  "strongboxSeal",
+  "brokenLuck",
+  "secondChance",
+  "wildMask"
+];
+
+export const PRACTICE_RUN_NOTICE = "practice run · these blocks are real, not proved this session";
