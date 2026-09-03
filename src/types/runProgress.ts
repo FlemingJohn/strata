13004dfbaef452f0ec4ca97272b09cfbf319174d
FlemingJohn <flemjohn08@gmail.com>
2026-09-03 20:12:17 +0530

    Add the run progress type

---
+++ src/types/runProgress.ts
@@ -0,0 +1,11 @@
+import type { EquippedRelic } from "./relic";
+
+export interface RunProgress {
+  provenRelics: EquippedRelic[];
+  equippedRelics: EquippedRelic[];
+  depth: number;
+  roomsClearedSoFar: number;
+  killsSoFar: number;
+  carriedHealth: number | null;
+  walletAddress: string;
+}
