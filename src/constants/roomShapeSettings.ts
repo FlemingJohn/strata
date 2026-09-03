29eec563e589f10ebd41a17884928ba62cd845a2
FlemingJohn <flemjohn08@gmail.com>
2026-09-03 21:04:49 +0530

    Add the sizes for each room shape

---
+++ src/constants/roomShapeSettings.ts
@@ -0,0 +1,5 @@
+export const HALL_PILLAR_INSET_TILES = 3;
+export const HALL_PILLAR_GAP_TILES = 5;
+export const PASSAGE_WALL_THICKNESS_TILES = 2;
+export const ARENA_CORNER_CUT_TILES = 3;
+export const VAULT_INSET_TILES = 6;
