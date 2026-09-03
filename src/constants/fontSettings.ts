af613deb8a328e28ede3038f6e9dbd4136e138b0
FlemingJohn <flemjohn08@gmail.com>
2026-09-03 19:59:37 +0530

    Name the pixel fonts and their design size

---
+++ src/constants/fontSettings.ts
@@ -0,0 +1,10 @@
+export const PIXEL_FONT_NAME = "Silkscreen";
+export const LOGO_FONT_NAME = "Press Start 2P";
+
+export const PIXEL_FONT_DESIGN_SIZE = 8;
+
+export const PIXEL_FONTS_TO_WAIT_FOR = [
+  `8px "${PIXEL_FONT_NAME}"`,
+  `16px "${PIXEL_FONT_NAME}"`,
+  `32px "${LOGO_FONT_NAME}"`
+];
