1e84d4c3c822ed1d69378b1d6cbda0d4ff8f982c
FlemingJohn <flemjohn08@gmail.com>
2026-09-03 20:12:19 +0530

    Add the shield, ally and revive settings

---
+++ src/constants/relicPowerSettings.ts
@@ -0,0 +1,8 @@
+export const HITS_BETWEEN_SUMMONED_ALLIES = 5;
+export const HEALTH_SHARE_ON_REVIVE = 0.3;
+
+export const ALLY_LIFETIME_SECONDS = 14;
+export const ALLY_ENEMY_NAME = "skeletonRogue";
+export const ALLY_HEALTH_SHARE = 0.6;
+export const ALLY_TINT_COLOUR = "#8FD0FF";
+export const ALLY_SPAWN_DISTANCE_PIXELS = 18;
