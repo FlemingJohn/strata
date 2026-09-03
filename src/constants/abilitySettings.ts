acf60847c950ca1984846cb009bf69d52567a1e7
FlemingJohn <flemjohn08@gmail.com>
2026-09-03 20:16:52 +0530

    Rename the nova settings to blast

--- src/constants/abilitySettings.ts
+++ src/constants/abilitySettings.ts
@@ -2,11 +2,11 @@ export const CAST_DURATION_SECONDS = 0.7;
 export const CAST_STAMINA_COST = 45;
 export const CAST_COOLDOWN_SECONDS = 3.5;
 
-export const NOVA_MAXIMUM_RADIUS_PIXELS = 74;
-export const NOVA_EXPAND_SECONDS = 0.45;
-export const NOVA_DAMAGE = 26;
-export const NOVA_SELF_DAMAGE = 6;
-export const NOVA_KNOCKBACK_SPEED = 260;
+export const BLAST_MAXIMUM_RADIUS_PIXELS = 74;
+export const BLAST_EXPAND_SECONDS = 0.45;
+export const BLAST_DAMAGE = 26;
+export const BLAST_SELF_DAMAGE = 6;
+export const BLAST_KNOCKBACK_SPEED = 260;
 
 export const SLAM_WIND_UP_SECONDS = 1.1;
 export const SLAM_MAXIMUM_RADIUS_PIXELS = 64;
@@ -14,6 +14,6 @@ export const SLAM_EXPAND_SECONDS = 0.35;
 export const SLAM_DAMAGE = 22;
 export const SLAM_COOLDOWN_SECONDS = 5;
 
-export const NOVA_COLOUR = "#C4523A";
+export const BLAST_COLOUR = "#C4523A";
 export const SLAM_COLOUR = "#E0A233";
 export const RING_THICKNESS_PIXELS = 2;
