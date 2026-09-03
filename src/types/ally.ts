4a5d96a0d78cd3557536cf802cd1cc2d4e724d2c
FlemingJohn <flemjohn08@gmail.com>
2026-09-03 20:12:21 +0530

    Add the ally type

---
+++ src/types/ally.ts
@@ -0,0 +1,8 @@
+export interface Ally {
+  horizontalPosition: number;
+  verticalPosition: number;
+  currentHealth: number;
+  secondsRemaining: number;
+  secondsUntilNextStrike: number;
+  isFacingLeft: boolean;
+}
